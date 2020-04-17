8cfa0ab279ff1065d5400a5d4169ee6b98a9873b
Sven Grunewaldt <strayer@olle-orks.org>
2020-04-17 13:51:40 +0200

    Try using MT with Enter on right shift

--- keyboards/ergodash/rev1/keymaps/strayer/keymap.c
+++ keyboards/ergodash/rev1/keymaps/strayer/keymap.c
@@ -16,6 +16,7 @@ enum custom_keycodes {
 
 /* #define HYESC MT(MOD_HYPR, KC_ESC) */
 #define HYESC LT(_HYPER, KC_ESC)
+#define ENTSFT MT(MOD_RSFT, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -24,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     HYESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB,                         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LALT, KC_LALT,     KC_LGUI,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, KC_ENT ,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENTSFT, \
+    KC_LCTL, KC_LALT, KC_LALT,     _______,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower */
@@ -33,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
     HYESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TAB,                         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LALT, KC_LALT,     KC_LGUI,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, KC_ENT ,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, ENTSFT, \
+    KC_LCTL, KC_LALT, KC_LALT,     _______,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Raise */
@@ -42,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     HYESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TAB,                         KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQT , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LALT, KC_LALT,     KC_LGUI,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, KC_ENT ,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,                         KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, ENTSFT, \
+    KC_LCTL, KC_LALT, KC_LALT,     _______,      KC_LGUI, KC_SPC, LOWER,           RAISE, KC_BSPC, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   [_HYPER] = LAYOUT(
